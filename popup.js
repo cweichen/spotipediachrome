73b24bdb024256d9600ebe6f5d513ba50fa44a46
chingweichen@gmail.com <chingweichen@gmail.com>
2015-03-20 14:54:43 +0100

    Updated to new Spotify API. Added Google Analytics

--- popup.js
+++ popup.js
@@ -1,15 +1,28 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-60990296-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = 'https://ssl.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 chrome.tabs.getSelected(null, function(tab) {
 	// Send message to content script to get artist name from page
 	chrome.tabs.sendMessage(tab.id, {greeting: "hello"}, function(response) {
-		
+
 		if (response) {
-			
+
 			// Hide search box
 			$("#search").hide();
-			
+
 			// Get artist name from page
 			var artist = response.farewell;
-	
+
+			// Analytics
+			_gaq.push(['_trackEvent', 'searchedArtist', artist]);
+
 			// Show progress indicator (using spin.js)
 			var opts = {
 			  lines: 13, // The number of lines to draw
@@ -30,51 +43,48 @@ chrome.tabs.getSelected(null, function(tab) {
 			};
 			var target = document.getElementById('player');
 			var spinner = new Spinner(opts).spin(target);
-	
+
 			// Get list of tracks by artist
 			var trackset = {};
-			$.getJSON('http://ws.spotify.com/search/1/track.json?page=1&q=artist:' + encodeURIComponent(artist), function (data) {
-			
+			$.getJSON('https://api.spotify.com/v1/search?type=track&q=artist:' + encodeURIComponent(artist), function (data) {
+
 				// Stop progress indicator
 				spinner.stop();
-			
+
 				// Get tracks
-				$.map(data.tracks, function(item){
+				$.map(data.tracks.items, function(item){
 					var trackname = item.name;
-					var uri = item.href.replace('spotify:track:','');
-					trackset[trackname] = uri;	// Using trackname as key will remove tracks with duplicate names
+					trackset[trackname] = item.id;	// Using trackname as key will remove tracks with duplicate names
 				});
-		
+
 				// Spotify will return up to 100 tracks, limit to 20 tracks
-				var uriset = '';
+				var idset = '';
 				var i = 0;
-				$.each(trackset, function (name,uri){
-					uriset += uri + ',';
+				$.each(trackset, function (name,id){
+					idset += id + ',';
 					i += 1;
 					if (i >= 20){
 						return false;
 					}
 				});
-		
+
 				// Insert player iframe
-				var playerurl = 'https://embed.spotify.com/?uri=spotify:trackset:' + encodeURIComponent(artist) + ':' + uriset;	
-				var embed = '<iframe id="spotify" src="' + playerurl + '" frameborder="0" allowtransparency="true"></iframe>';	
+				var playerurl = 'https://embed.spotify.com/?uri=spotify:trackset:' + encodeURIComponent(artist) + ':' + idset;
+				var embed = '<iframe id="spotify" src="' + playerurl + '" frameborder="0" allowtransparency="true"></iframe>';
 				$("#player").html(embed);
-		
+
 				// Insert Tweet iframe
 				var tweet_text = "I'm listening to " + artist + " on Wikipedia with @spotipedia";
 				var spotipedia_url = "https://chrome.google.com/webstore/detail/hgfjkidofjemkpgfggfpebolkbhagmbf";
 				var tweet_url = 'https://platform.twitter.com/widgets/tweet_button.html?count=none&text=%E2%99%AB%20' + encodeURIComponent(tweet_text) + '&url=' + encodeURIComponent(spotipedia_url);
 				var tweet_iframe = '<iframe id="tweet_iframe" allowtransparency="true" frameborder="0" scrolling="no" src="' + tweet_url + '"></iframe>';
 				$("#tweet").html(tweet_iframe);
-				
+
 				// Insert Like iframe
 				var like_iframe = '<iframe id="like_iframe" src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FSpotipedia%2F382108901861180&amp;send=false&amp;layout=button_count&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;appId=119716694842043" scrolling="no" frameborder="0" allowTransparency="true"></iframe>';
 				$("#like").html(like_iframe);
-		
+
 			});
 		}
 	});
 });
-
-
